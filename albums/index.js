6e66e8f7b5a92aefeca0daaf0307a05f24c0d872
evagolabek <golabek.eva@gmail.com>
2018-09-21 13:56:07 +0200

    refactor of functional component to class based component

--- albums/index.js
+++ albums/index.js
@@ -4,7 +4,7 @@ import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
 import AlbumList from './src/components/AlbumList';
 
-//create a component
+//create a component (functional component i.e. functions that return some jsx to be displayed)
 const App = () => (
   <View>
     <Header headerText={'Albums'} />
@@ -15,3 +15,16 @@ const App = () => (
 
 //render it to the device (only the root component (App) use the AppRegistry)
 AppRegistry.registerComponent('albums', () => App);
+
+
+// FUNCTIONAL COMPONENT
+// used for representing static data
+// can't handle fetching data
+//easy to write e.g header component
+
+// CLASS COMPONENT
+// more complex with more code
+// more functionality and capability
+// can be used for fetching data
+// easier to write large components
+// class based because its based on ES6 classes
--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,12 +1,28 @@
-import React from 'react';
+
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>Album List</Text>
-    </View>
-  );
-};
+class AlbumList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Album List</Text>
+      </View>
+    );
+  }
+}
 
 export default AlbumList;
+
+// by using the extends Component we borrow bunch of functionality from this based component class
+// component based class can be imported from the react library
+// but you have to keep React in the scope of this file
+// reason for that is, when Babel goes through it and converts the jsx into raw js
+// it turns all jsx into series of react.element calls
+
+// so to both import React but also d structure the Component class out of it we do it as above
+
+// whenever you write class based component you must define exactly one method (render method)
+// the only requirement of the render method is to return some amount of jsx
+
+// classes don't need semicolon at the end
