4eb6292092011e6a78c25e2ce41244fb1464e149
evagolabek <golabek.eva@gmail.com>
2018-09-22 23:02:01 +0200

    position of elements with flexbox and dstructure props argument

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,20 +1,35 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+// when you have more (e.g. x 3 like below) references to props
+// its good convention to d-structure it out of argument of props
+// (props) turns into ({album})
+// and because you use album reference (x3) you need to d-structure as well
+// you can d-structure it directly next to ({album}) or list below in const/ more tidy
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image } = album;
   return (
     <Card>
       <CardSection>
-        <View></View>
         <View>
-          <Text>{props.album.title}</Text>
-          <Text>{props.album.artist}</Text>
+          <Image source={{ url: thumbnail_image }} />
+        </View>
+        <View style={styles.headerContentStyle}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
         </View>
       </CardSection>
     </Card>
   );
+};
+
+const styles = {
+    headerContentStyle: {
+      flexDirection: 'column',
+      justifyContent: 'space-around'
+    }
 };
 
 export default AlbumDetail;
