20e7a07e08e992a238e3e2df593bc7aa2ede1eef
evagolabek <golabek.eva@gmail.com>
2018-09-22 23:08:17 +0200

    image and refactor of styles

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -10,13 +10,18 @@ import CardSection from './CardSection';
 // you can d-structure it directly next to ({album}) or list below in const/ more tidy
 const AlbumDetail = ({ album }) => {
   const { title, artist, thumbnail_image } = album;
+  const { thumbnailStyle, headerContentStyle } = styles;
+
   return (
     <Card>
       <CardSection>
         <View>
-          <Image source={{ url: thumbnail_image }} />
+          <Image
+            style={thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+          />
         </View>
-        <View style={styles.headerContentStyle}>
+        <View style={headerContentStyle}>
           <Text>{title}</Text>
           <Text>{artist}</Text>
         </View>
@@ -25,10 +30,15 @@ const AlbumDetail = ({ album }) => {
   );
 };
 
+//image will not appear until you add style for it
 const styles = {
     headerContentStyle: {
       flexDirection: 'column',
       justifyContent: 'space-around'
+    },
+    thumbnailStyle: {
+      height: 50,
+      width: 50
     }
 };
 
