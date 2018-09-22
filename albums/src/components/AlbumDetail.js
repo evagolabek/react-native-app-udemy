ea4718d8f5ef7099f068a9290323dd766825bd9e
evagolabek <golabek.eva@gmail.com>
2018-09-22 23:36:23 +0200

    album cover img card

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -9,37 +9,63 @@ import CardSection from './CardSection';
 // and because you use album reference (x3) you need to d-structure as well
 // you can d-structure it directly next to ({album}) or list below in const/ more tidy
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
-  const { thumbnailStyle, headerContentStyle } = styles;
+  const { title, artist, thumbnail_image, image } = album;
+  const { thumbnailStyle,
+          headerContentStyle,
+          thumbnailContainerStyle,
+          headerTextStyle,
+          imageStyle
+        } = styles;
 
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image
             style={thumbnailStyle}
             source={{ uri: thumbnail_image }}
           />
         </View>
         <View style={headerContentStyle}>
-          <Text>{title}</Text>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          style={imageStyle}
+          source={{ uri: image }} />
       </CardSection>
     </Card>
   );
 };
 
 //image will not appear until you add style for it
+// width to null ?
 const styles = {
     headerContentStyle: {
       flexDirection: 'column',
       justifyContent: 'space-around'
     },
+    headerTextStyle: {
+      fontSize: 18,
+    },
     thumbnailStyle: {
       height: 50,
       width: 50
+    },
+    thumbnailContainerStyle: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginLeft: 10,
+      marginRight: 10
+    },
+    imageStyle: {
+      height: 300,
+      flex: 1,
+      width: null
     }
+
 };
 
 export default AlbumDetail;
